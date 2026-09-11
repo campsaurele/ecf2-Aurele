e61baf5974fc66d55a0528dcd453ba62c42b2722
campsaurele <campsaurele@gmail.com>
2026-09-11 15:05:12 +0200

    Change : Migration to clearout db making

--- migrations/Version20260911095715.php
+++ migrations/Version20260911095715.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260908074410 extends AbstractMigration
+final class Version20260911095715 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,10 +20,11 @@ final class Version20260908074410 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE absences (id INT AUTO_INCREMENT NOT NULL, date_time DATE NOT NULL, reason_id INT NOT NULL, trainee_id INT DEFAULT NULL, INDEX IDX_F9C0EFFF59BB1592 (reason_id), INDEX IDX_F9C0EFFF36C682D0 (trainee_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE absences (id INT AUTO_INCREMENT NOT NULL, date_time DATE NOT NULL, document VARCHAR(255) DEFAULT NULL, reason_id INT NOT NULL, trainee_id INT DEFAULT NULL, INDEX IDX_F9C0EFFF59BB1592 (reason_id), INDEX IDX_F9C0EFFF36C682D0 (trainee_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE reasons (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('CREATE TABLE trainee (id INT AUTO_INCREMENT NOT NULL, lastname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, training_id INT NOT NULL, INDEX IDX_46C68DE7BEFD98D1 (training_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE trainee (id INT AUTO_INCREMENT NOT NULL, lastname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, photo VARCHAR(255) DEFAULT NULL, training_id INT NOT NULL, INDEX IDX_46C68DE7BEFD98D1 (training_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE training (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, login VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_LOGIN (login), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('ALTER TABLE absences ADD CONSTRAINT FK_F9C0EFFF59BB1592 FOREIGN KEY (reason_id) REFERENCES reasons (id)');
         $this->addSql('ALTER TABLE absences ADD CONSTRAINT FK_F9C0EFFF36C682D0 FOREIGN KEY (trainee_id) REFERENCES trainee (id)');
@@ -40,6 +41,7 @@ final class Version20260908074410 extends AbstractMigration
         $this->addSql('DROP TABLE reasons');
         $this->addSql('DROP TABLE trainee');
         $this->addSql('DROP TABLE training');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
